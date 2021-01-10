aa2d905ea4d3bbd146be50ee3e3d7b5b4ff4a487
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-09 20:41:33 -0500

    variable declarations for prompts, booleans

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ function createPassword() {
   while (Number.isNaN(length) || length < 8 || length > 128) {
     length = Number(prompt("Keep it between 8 and 128 characters."));
 
+    //Variable Declarations for Prompts
+    var uppercase = confirm("Do you want uppercase letters?");
+    var lowercase = confirm("Do you want lowercase letters?");
+    var numbers = confirm("Do you want numbers?");
+    var special = confirm("Do you want special characters?");
+
+    // If false, message will appear and return to variable declarations. If return doesn't work, add variables back
+    while (!uppercase && !lowercase && !numbers && !special) {
+      alert("Please select one criteria.");
+      uppercase = confirm("Do you want uppercase letters?");
+      lowercase = confirm("Do you want lowercase letters?");
+      numbers = confirm("Do you want numbers?");
+      special = confirm("Do you want special characters?");
+    }
+
 }
 
 
