1283d665661df752093bf35a33e763ad37a3a30e
dmorgan904 <demetriusimorgan@gmail.com>
2021-01-09 20:40:06 -0500

    create prompt for password length

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 // Assignment code here
 
+//Button
+//var generateButton = document.getElementById('generateNewPassord');
+//generateButton.addEventListener('click', function() {
+  //generateNewPassword();
+//});
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+// Prompt for password length
+function createPassword() {
+  var length = Number(prompt("How long would you like your password to be? It must be at least 8 characters."));
+  while (Number.isNaN(length) || length < 8 || length > 128) {
+    length = Number(prompt("Keep it between 8 and 128 characters."));
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword); 
+} 
